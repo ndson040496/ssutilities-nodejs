ec140ca4556fe29f88e291937747095da5d7c9a0
Son Nguyen <nguy2565@umn.edu>
2020-12-22 23:26:54 +0700

    added file reader

--- src/common-functions/index.js
+++ src/common-functions/index.js
@@ -1,5 +1,32 @@
 const versionCompare = require('./version-compare');
+const fs = require('fs');
+
+async function readPlainFile(file) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, 'utf8', (error, data) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(data);
+            }
+        })
+    });
+}
+
+async function readJsonFile(file) {
+    return new Promise(resolve => {
+        readPlainFile(file).then(string => {
+            return JSON.parse(string);
+        }).catch(error => {
+            resolve(null);
+        })
+    });
+}
 
 module.exports = {
-    compareVersions: versionCompare
+    compareVersions: versionCompare,
+    fileReader: {
+        readPlainFile: readPlainFile,
+        readJsonFile: readJsonFile
+    }
 }
